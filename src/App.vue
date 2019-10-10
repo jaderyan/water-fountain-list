13cb18b461b6cc43141bd815bfc223b1f4a095b2
jaderyan <jadeelizabethryan@outlook.com>
2019-10-10 19:04:47 +0100

    make header into own component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <section class="hero is-info is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1">Water Fountains</h1>
-        </div>
-      </div>
-    </section>
+    <app-header />
     <main class="section">
       <div class="container">
         <div v-for="fountain in fountains" v-bind:key="fountain.id">
@@ -20,6 +14,7 @@
 
 <script>
 import Fountain from "./components/Fountain";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import API from "./service/api";
@@ -28,7 +23,8 @@ export default {
   name: "app",
   components: {
     Fountain,
-    "app-footer": Footer
+    "app-footer": Footer,
+    "app-header": Header
   },
   data() {
     return {
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="hero is-info is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">Water Fountains</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "app-header"
+};
+</script>
